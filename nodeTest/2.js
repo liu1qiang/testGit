c873b23cbdc4be714d41ea72874469b5c9ed687c
liu1qiang <646126265@qq.com>
2017-06-21 15:32:30 +0800

    ceshi
    新增a方法

--- nodeTest/2.js
+++ nodeTest/2.js
@@ -2,7 +2,11 @@
 * @Author: Administrator
 * @Date:   2017-06-21 11:23:06
 * @Last Modified by:   Administrator
-* @Last Modified time: 2017-06-21 11:30:45
+* @Last Modified time: 2017-06-21 15:30:18
 */
 
 'use strict';
+function a(param){
+	console.log(param)
+};
+a();
