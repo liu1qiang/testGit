ef8e2b9f254a5fda1f345e311a9f2ca5a3a67a8d
liu1qiang <646126265@qq.com>
2017-06-21 16:54:42 +0800

    q
    s

--- nodeTest/2.js
+++ nodeTest/2.js
@@ -2,10 +2,9 @@
 * @Author: Administrator
 * @Date:   2017-06-21 11:23:06
 * @Last Modified by:   Administrator
-* @Last Modified time: 2017-06-21 15:30:18
+* @Last Modified time: 2017-06-21 16:54:33
 */
 
-'use strict';
 function a(param){
 	console.log(param)
 };
